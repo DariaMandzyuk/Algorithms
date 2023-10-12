079c0f50ffb8a3e4d54ef73cddb73eaefc06ae17
Daria Mandziuk <dariacolorso@gmail.com>
2023-10-12 15:17:01 +0400

    add BubbleSort

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
     println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+    val array = intArrayOf(1,6,8,4,3,9,10,56,4)
+    sort(array)
+    println(array.contentToString())
 }
